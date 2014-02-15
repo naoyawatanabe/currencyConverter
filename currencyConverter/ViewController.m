6ac50e6a4bbd85507408a40ff68b1eed77a2beb8
nao <nao@nao-no-MacBook-Air.local>
2014-02-16 08:55:04 +0900

    Initial Commit

---
+++ currencyConverter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  currencyConverter
+//
+//  Created by nao on 2014/02/16.
+//  Copyright (c) 2014年 showandtell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
